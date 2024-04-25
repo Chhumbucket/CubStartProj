8bfc81b55eb5b0271bfe8c90a9c5f921916304a2
Chhumbucket <dylchhum@gmail.com>
2024-04-24 23:16:00 -0700

    User View

---
+++ TurnPages/TurnPages/UserView.swift
@@ -0,0 +1,87 @@
+//
+//  UserView.swift
+//  TurnPages
+//
+//  Created by Dylan Chhum on 4/19/24.
+//
+
+import SwiftUI
+
+struct UserView: View {
+    enum Tab {
+        case books, reviews, journals
+    }
+    
+    @State private var selectedTab: Tab = .books
+    
+    
+    var body: some View {
+        VStack {
+            Picker(selection: $selectedTab, label: Text("Tab")) {
+                Text("Books").tag(Tab.books)
+                Text("Reviews").tag(Tab.reviews)
+                Text("Personal Journal").tag(Tab.journals)
+            }
+            
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
+            
+            Image(systemName: "person.crop.circle")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+            
+            NavigationView {
+                List{
+                    NavigationLink(destination: UserActivity()) {
+                        Text("Film")
+                    }
+                    Text("Hi")
+                    
+                }
+            }
+            HStack {
+                Button(action: {
+                    // Main Menu Action
+                }) {
+                    Image(systemName: "house")
+                    Text("Main Menu")
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button(action: {
+                    // Profile Action
+                }) {
+                    Image(systemName: "person")
+                    Text("Profile")
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button(action: {
+                    // Search Action
+                }) {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+                .padding()
+            }
+            .background(Color.gray.opacity(0.2))
+            
+        }
+    }
+}
+
+struct UserActivity: View {
+    var body: some View {
+        Text("VIew")
+    }
+    
+}
+
+#Preview {
+    UserView()
+}
